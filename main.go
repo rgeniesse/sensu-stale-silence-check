c189548f916ccbfcbfc911cb7a816d4a18378444
Richard Geniesse <richard.geniesse@gmail.com>
2019-09-17 00:01:22 -0700

    various updates

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+// Todo:
+// Add command line paremeter pass in threshold, API credentials, API host, and API port.
+// Add looping logic for silenced end points with more than one entry.
+// Add data structure to hold all endpoints found to be too old.
+// Make exit status based on if that data structure has entries or not.
+// Add check if silenced is empty as program crashes with empty array.
+
 package main
 
 import (
@@ -6,6 +13,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -37,7 +45,7 @@ func getAuthToken() string {
 	passwd := "P@ssw0rd!"
 
 	client := &http.Client{Timeout: 10 * time.Second}
-	req, err := http.NewRequest("GET", "http://172.28.128.14:8080/auth", nil)
+	req, err := http.NewRequest("GET", "http://172.28.128.15:8080/auth", nil)
 	req.SetBasicAuth(username, passwd)
 	resp, err := client.Do(req)
 	if err != nil {
@@ -54,12 +62,11 @@ func getAuthToken() string {
 	return myauth.Access_token
 }
 
-func querySilenced(token string) {
+func querySilenced(token string, silenced2 *[]Silenced) {
 
-	silenced := []Silenced{}
 	bearer := "Bearer " + token
 	client := &http.Client{Timeout: 10 * time.Second}
-	req, err := http.NewRequest("GET", "http://172.28.128.14:8080/api/core/v2/namespaces/default/silenced", nil)
+	req, err := http.NewRequest("GET", "http://172.28.128.15:8080/api/core/v2/namespaces/default/silenced", nil)
 	req.Header.Add("Authorization", bearer)
 	resp, err := client.Do(req)
 	if err != nil {
@@ -69,24 +76,46 @@ func querySilenced(token string) {
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-	bleh := json.Unmarshal([]byte(body), &silenced)
+	bleh := json.Unmarshal([]byte(body), silenced2)
 
 	if bleh != nil {
 		panic(bleh)
 	}
-	// fmt.Printf("%v\n", silenced)
-	fmt.Printf("%v\n", silenced[0].Begin)
-	fmt.Printf("%v\n", silenced[0].Check)
-	fmt.Printf("%v\n", silenced[0].Creator)
-	fmt.Printf("%v\n", silenced[0].Expire)
-	fmt.Printf("%v\n", silenced[0].Expire_on_resolve)
-	fmt.Printf("%v\n", silenced[0].Subscription)
-	fmt.Printf("%v\n", silenced[0].Metadata.Name)
-	fmt.Printf("%v\n", silenced[0].Metadata.Namespace)
+}
+
+func checkIfSilencedOld(t int64, time_treshold int) {
+	n := time.Unix(t, 0)
+	fmt.Println(n)
+	duration := time.Since(n)
+	fmt.Println(duration.Seconds())
+
+	if int(duration.Seconds()) > time_treshold {
+		// Make a data structure that holds all the entries. Use that data structure
+		// to determine the exit status, otherwise program exits on first hit.
+		fmt.Println("This entry is old and was added to check result!")
+		os.Exit(1)
+	} else {
+		fmt.Println("Entry was not added to check result")
+	}
 }
 
 func main() {
 	fmt.Println("requesting...")
 	token := getAuthToken()
-	querySilenced(token)
+	silenced := []Silenced{}
+	querySilenced(token, &silenced)
+	time_treshold := 109000
+
+	// fmt.Printf("%v\n", silenced)
+	// fmt.Printf("%v\n", silenced[0].Begin)
+	// fmt.Printf("%v\n", silenced[0].Check)
+	// fmt.Printf("%v\n", silenced[0].Creator)
+	// fmt.Printf("%v\n", silenced[0].Expire)
+	// fmt.Printf("%v\n", silenced[0].Expire_on_resolve)
+	// fmt.Printf("%v\n", silenced[0].Subscription)
+	// fmt.Printf("%v\n", silenced[0].Metadata.Name)
+	// fmt.Printf("%v\n", silenced[0].Metadata.Namespace)
+
+	checkIfSilencedOld(int64(silenced[0].Begin), time_treshold)
+
 }
