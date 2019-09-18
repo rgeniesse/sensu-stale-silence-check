8103f1e09e856c1e979a01ddf7c8acc45a471974
Richard Geniesse <richard.geniesse@gmail.com>
2019-09-17 23:25:55 -0700

    gg checkpoint

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// Todo:
-// Add data structure to hold all endpoints found to be too old.
-
 package main
 
 import (
@@ -142,28 +139,23 @@ func querySilenced(token string, silenced2 *[]Silenced) {
 
 func checkIfSilencedOld(silenced3 []Silenced) {
 
-	fmt.Println(silenced3[0].Begin)
-	fmt.Printf("%T\n", silenced3)
-	fmt.Println(silenced3)
-
 	if len(silenced3) > 0 {
 		for i := 0; i < len(silenced3); i++ {
 			n := time.Unix(int64(silenced3[i].Begin), 0)
 			duration := time.Since(n)
 
-			fmt.Println(silenced3[i])
-			if int(duration.Seconds()) > threshold {
-				// Make a data structure that holds all the entries. Use that data structure
-				// to determine the exit status, otherwise program exits on first hit.
+			if int(duration.Seconds()) > threshold && silenced3[i].Expire == int(-1) && !silenced3[i].Expire_on_resolve {
+				// Need better output here that calls out entity and silence information
 				fmt.Println("This entry is old and was added to check result!")
-			} else {
-				fmt.Println("Entry was not added to check result")
+				// split into more conditionals so the proper message is printed or logged.
+			} else if int(duration.Seconds()) < threshold || silenced3[i].Expire != int(-1) || silenced3[i].Expire_on_resolve {
+				fmt.Println("Entry was not added to check result because threshold not met, silence set to expire after some time or silence to expire on resolve")
 			}
 
 		}
 		os.Exit(1)
 	} else {
-		fmt.Println("Silenced endpoint is empty!")
+		fmt.Println("Good news nobody, the silenced endpoint is empty!")
 		os.Exit(0)
 	}
 }
@@ -174,10 +166,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Requesting Auth Token")
 	token := getAuthToken()
 	silenced := []Silenced{}
-	fmt.Printf("%T\n", silenced)
 	querySilenced(token, &silenced)
 	checkIfSilencedOld(silenced)
 
